d81c266c7d88ede2ab29db69d5db52379821d84e
kawakamir <rui.kawakami@gmail.com>
2018-08-24 20:03:14 +0900

    doing migrate

---
+++ school/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Person(models.Model):
+
+  MAN = 0
+  WOMAN = 1
+
+  name = models.CharField(max_length=128)
+  age = models.IntegerField()
+  sex = models.IntegerField(editable=False)
+
+class Lesson(models.Model):
+
+  ENGLISH = 0
+  PROGRAMMING = 1
+  FINANCE = 2
+
+  person = models.ForeignKey('Person',on_delete=models.CASCADE)
+  joined_at = models.DateTimeField()
+  lesson_category = models.IntegerField()
+  lesson_time = models.IntegerField()
